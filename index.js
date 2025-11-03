22dc2909c23047380c3941e5e92a6ca61fad070b
SoumenCodes <soumen19j@gmail.com>
2025-11-03 22:33:15 +0530

    post

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // ----------------------------
 require("dotenv").config(); // Load .env variables first
-const express = require("express");
 const helmet = require("helmet"); // Secures HTTP headers
 const cors = require("cors"); // Manages Cross-Origin Resource Sharing
 const rateLimit = require("express-rate-limit"); // Prevents brute-force attacks
@@ -17,7 +16,7 @@ app.use(helmet()); // Set secure headers
 // --- CORS Configuration ---
 // Only allow your Next.js frontend to make requests
 const corsOptions = {
-  origin: "http://localhost:3000", // Your Next.js app URL
+  origin: "http://localhost:3001", // Your Next.js app URL
 };
 app.use(cors(corsOptions));
 
@@ -42,8 +41,37 @@ const authLimiter = rateLimit({
 // app.use("/api/auth", authRoutes);
 // app.use("/api/data", protectedRoutes);
 
+const alldataArray = [
+  { pass: 1, name: "Item 1" },
+  { pass: 2, name: "Item 2" },
+  { pass: 3, name: "Item 3" },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello World! for " + port);
+  res.json({
+    msg: "HI to head route",
+    data: alldataArray,
+  });
+});
+
+app.get("/head", (req, res) => {
+  res.json({
+    msg: "HI to head route",
+  });
+  // res.send("Hi");
+});
+app.post("/addData", (req, res) => {
+  const name = req.body.name;
+  const pass = req.body.pass;
+  const data = { name, pass };
+  console.log(data);
+  alldataArray.push(data);
+
+  res.json({
+    msg: "HI to head route",
+  });
+  // res.send("Hi");
 });
 
 app.listen(port, () => {
